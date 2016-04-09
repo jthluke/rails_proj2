8ac66f1bd4ce55dd0eb63af7192c8a2a7b5893fc
Justin Luke <justin.luke@berkeley.edu>
2016-04-09 01:45:58 -0700

    Resources in routes.rb; added relation in post.rb

--- proj2/config/routes.rb
+++ proj2/config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
+  #resources :Department, only [:show]
+  #resources :Course, only [:show]
+  #resources :Topic, only [:show, :edit, :update, :create, :destroy]
+  #resources :Post, only [:show, :edit, :update, :create, :destroy]
+  #resources :Tag, only [:show]
+  #resources :User, only [:show]
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
